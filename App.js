c8fea77f77dcb1178fb5377105737f9bbcad3507
test <Oleg Bulychev>
2021-08-23 11:37:56 +0000

    2

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          TESTETSTESTTo get started, edit <code>src/App.js</code> and save to reload.
+          12312312312313123To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );

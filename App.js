66d8c2ee5fa76ad171357cad76ec7ff7118bb117
test <Oleg Bulychev>
2021-08-24 09:35:33 +0000

    123

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          TRIGGERED12312312312313123To get started, edit <code>src/App.js</code> and save to reload.
+          PRIVET To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
